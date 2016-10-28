66dca58cbe1ad6cab74c11d179c16f2ec0b67280
Optixal <optixal@live.com.sg>
2016-10-29 01:49:44 +0800

    Modified install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 rm -f vimrc
 git pull
-git checkout origin/master vimrc
+git fetch --all
+git reset --hard origin/master
 ln -s -f vimrc ~/.vimrc
 git submodule update --init
 echo "Vim configurations installed/updated successfully!"
