80ab5c792de257d148c43633cd5638e07410776d
Optixal <optixal@live.com.sg>
2016-12-28 00:06:34 +0800

    Updated wordings in install and update scripts

--- install.sh
+++ install.sh
@@ -10,4 +10,4 @@ git clone https://github.com/powerline/fonts /tmp/powerline-fonts
 /tmp/powerline-fonts/install.sh
 rm -rf /tmp/powerline-fonts
 
-echo -e '\n\e[32m[\e[1m\e[92m+\e[0m\e[32m] \e[1m\e[92mSuccessfully installed Optixal'\''s vimrc!\e[0m\n'
+echo -e '\n\e[32m[\e[1m\e[92m+\e[0m\e[32m] \e[1m\e[92mSuccessfully installed vimrc!\e[0m\n'
--- update.sh
+++ update.sh
@@ -8,4 +8,4 @@ ln -s -f vimrc ~/.vimrc
 git submodule update --init
 git submodule foreach git pull origin master
 
-echo -e '\n\e[32m[\e[1m\e[92m+\e[0m\e[32m] \e[1m\e[92mSuccessfully updated Optixal'\''s vimrc!\e[0m\n'
+echo -e '\n\e[32m[\e[1m\e[92m+\e[0m\e[32m] \e[1m\e[92mSuccessfully updated vimrc!\e[0m\n'
